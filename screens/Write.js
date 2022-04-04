3553bf1a51ac299a9209df37e8e2311432ee7cb6
yblim <dudqls28@naver.com>
2022-04-04 14:20:45 +0900

    add navi,screen

---
+++ screens/Write.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const View = styled.View`
+
+`;
+
+const Text = styled.Text`
+
+`;
+
+const Write = () => (
+    <View>
+        <Text>Write!</Text>
+    </View>
+);
+export default Write;
